3c8ea4f1bc27d5af80759e08239e62a0a7de87b6
LoppA <xLopacheco@gmail.com>
2017-12-13 01:19:37 -0200

    a

--- TSP_AG.cpp
+++ TSP_AG.cpp
@@ -5,17 +5,20 @@
 #include <cstdlib>
 #include <ctime>
 #include <cstring>
+#include <cmath>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
 double dist[POP][POP];
 
-// O(N**2)
 Gen::Gen() {
 	for (int i = 0; i < N; i++)
 		pos[i] = i;
 	random_shuffle(pos, pos + N);
+
+	rate();
 }
 
 void Gen::print () {
@@ -40,7 +43,73 @@ void Gen::operator = (const Gen &other) {
 	this->fitness = other.fitness;
 }
 
+bool Gen::operator < (const Gen &other) const {
+	return this->fitness > other.fitness;
+}
+
 Gen pop[POP], best;
+
+bool raffle (double prob) {
+	return rand() < ceil(prob * RAND_MAX);
+}
+
+bool vis[POP];
+Gen cross (Gen mama, Gen papa) {
+	Gen child;
+
+	memset (vis, 0, sizeof vis);
+	for (int i = 0; i < POP; i++) {
+		child.pos[i] = -1;
+		if (!vis[mama.pos[i]] and !vis[papa.pos[i]]) {
+			if (raffle(0.5))	child.pos[i] = mama.pos[i];
+			else				child.pos[i] = papa.pos[i];
+		} else if (!vis[mama.pos[i]]) {
+			child.pos[i] = mama.pos[i];
+		} else if (!vis[papa.pos[i]]) {
+			child.pos[i] = papa.pos[i];
+		} 
+
+		if (child.pos[i] != -1)
+			vis[child.pos[i]] = true;
+	}
+
+	vector <int> unused;
+	for (int i = 0; i < POP; i++)
+		if (!vis[i])
+			unused.push_back(i);
+	random_shuffle(unused.begin(), unused.end());
+
+	int j = 0;
+	for (int i = 0; i < POP; i++)
+		if (child.pos[i] == -1)
+			child.pos[i] = unused[j++];
+
+	child.rate();
+	return child;
+}
+
+void reproduction () {
+	sort (pop, pop + POP);
+
+	vector <Gen> parents, next_gen;
+
+	for (int i = 0; i < POP; i++) {
+		double chance = (double)(i + 1) / (double)POP;
+//		double chance = pop[i].fitness / (double)pop[POP-1].fitness;
+		if (raffle(chance)) 
+			parents.push_back(pop[i]);
+	}
+
+	while (next_gen.size() < POP) {
+		Gen mama = parents[rand()%parents.size()];
+		Gen papa = parents[rand()%parents.size()];
+
+		next_gen.push_back(cross(mama, papa));
+	}
+
+	for (int i = 0; i < POP; i++)
+		pop[i] = next_gen[i];
+}
 
 int main (int argc, char *argv[]) {
 	srand(time(NULL));
@@ -49,14 +118,24 @@ int main (int argc, char *argv[]) {
 		for (int j = 0; j < POP; j++) 
 			scanf ("%lf", &dist[i][j]);
 
-	best = pop[0];
 	for (int i = 0; i < POP; i++)
-		if (pop[i].fitness > best.fitness)
+		pop[i] = Gen();
+
+	best = pop[0];
+	for (int i = 0; i < POP; i++) 
+		if (pop[i].fitness < best.fitness)
 			best = pop[i];
 
 	int gen = GEN;
 	while (gen--) {
+		for (int i = 0; i < POP; i++)
+			printf ("%lf\n", pop[i].fitness);
+		printf ("   Best: %lf\n\n", best.fitness);
+		reproduction();
 	}
+
+	best.print();
+	printf ("%lf\n", best.fitness);
 
 	return 0;
 }
--- TSP_AG.h
+++ TSP_AG.h
@@ -2,8 +2,8 @@
 #define __TSP_AG_H__
 
 #define N 10
-#define POP 10
-#define GEN 10
+#define POP 5
+#define GEN 100
 
 struct Gen {
 	int pos[N];
@@ -16,6 +16,12 @@ struct Gen {
 	double rate ();
 
 	void operator = (const Gen &other);
+
+	bool operator < (const Gen &other) const;
 };
+
+bool raffle (double);
+Gen cross (Gen, Gen);
+void reproduction();
 
 #endif
